7ca7cc2ca703976f0ee40b5f202c31692dac2880
Vegasana <venkatasatya.vegasana@cognizant.com.com>
2022-05-06 16:09:14 -0400

    my commit with the git add added

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("fs_s3fs/_version.py") as f:
+    exec(f.read())
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: System :: Filesystems",
+]
+
+with open("README.rst", "rt") as f:
+    DESCRIPTION = f.read()
+
+REQUIREMENTS = ["boto3~=1.9", "fs~=2.4", "six~=1.10"]
+
+setup(
+    name="fs-s3fs",
+    author="Will McGugan",
+    author_email="willmcgugan@gmail.com",
+    classifiers=CLASSIFIERS,
+    description="Amazon S3 filesystem for PyFilesystem2",
+    install_requires=REQUIREMENTS,
+    license="MIT",
+    long_description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=["pyfilesystem", "Amazon", "s3"],
+    platforms=["any"],
+    test_suite="nose.collector",
+    url="https://github.com/PyFilesystem/s3fs",
+    version=__version__,
+    entry_points={"fs.opener": ["s3 = fs_s3fs.opener:S3FSOpener"]},
+)
